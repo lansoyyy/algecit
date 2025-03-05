c3409655f4ae50f73f6dd6ba637955c2dd774687
Lans12345 <olanalans12345@gmail.com>
2025-03-05 10:49:38 +0800

    fixed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -414,10 +414,16 @@ class _HomeScreenState extends State<HomeScreen> {
                                           : TextWidget(
                                               text: DateFormat(
                                                       "yyyy-MM-dd hh:mm:ss a")
-                                                  .format(DateFormat(
-                                                          "dd-MM-yyyy : HH:mm:ss")
-                                                      .parse(toolData.docs[i]
-                                                          ['TimeStamp'])),
+                                                  .format(
+                                                DateFormat(
+                                                        "dd-MM-yyyy : HH:mm:ss")
+                                                    .parse(toolData.docs[i]
+                                                        ['TimeStamp'])
+                                                    .toUtc()
+                                                    .add(Duration(
+                                                        hours:
+                                                            8)), // Convert to UTC+8
+                                              ),
                                               fontSize: 12,
                                               fontFamily: 'Regular',
                                               color: Colors.black,
